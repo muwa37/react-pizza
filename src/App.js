62e23cef1bf157c8ab6dbd30d697316b903bee71
muwa37 <yond41m3@icloud.com>
2024-02-27 22:23:15 +0600

    Added fetching and mockapi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useEffect, useState } from 'react';
 import Categories from './components/Categories';
 import Header from './components/Header';
 import PizzaBlock from './components/PizzaBlock';
 import Sort from './components/Sort';
 import './scss/app.scss';
 
-import pizzas from './assets/pizzas.json';
-
 function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch('https://65de02ccdccfcd562f561234.mockapi.io/items').then(res =>
+      res.json().then(json => setItems(json))
+    );
+  }, []);
+
   return (
     <div>
       <div className='wrapper'>
@@ -19,7 +26,7 @@ function App() {
             </div>
             <h2 className='content__title'>Все пиццы</h2>
             <div className='content__items'>
-              {pizzas.map(pizza => (
+              {items.map(pizza => (
                 <PizzaBlock key={pizza.id} {...pizza} />
               ))}
             </div>
